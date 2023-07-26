c6f8acad5358a6703422d90ce6069ada30a40a9e
mirafzal114 <mirafzaldevelop@gmail.com>
2023-07-26 18:50:42 +0500

    you should enter your data base passowrd and database name

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 host = 'localhost'
-user = 'postgres'
-password = 'mirafzal'
-db_name = 'postgres'
+user = input('enter your user: ')
+password = input('enter your password on database: ')
+db_name = input("enter your data base name: ")
